896d3761f5e8821b8441d5b3f73d2a680892ff87
Alexis Edwards <56770721+aedwards4@users.noreply.github.com>
2021-11-16 14:14:36 -0600

    Finished Checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -7,7 +7,7 @@ class Checkout extends React.Component {
     render() {
         return (
             <div className="checkout">
-                <Header cartCount='2'/>
+                <Header cartCount='3'/>
                 <div className="checkout-header">Checkout</div>
                 <div className="checkout-main">
                 <div className="payment-details">
@@ -27,11 +27,23 @@ class Checkout extends React.Component {
                             <input type="text" id="fname" name="fname" placeholder="First Name"></input>
                             <input type="text" id="lname" name="lname" placeholder="Last Name"></input>
                             <input type="text" id="email" name="email" placeholder="Email"></input><br/>
-                            <input type="text" id="address1" name="address1" placeholder="Address Line 1"></input>
-                            <input type="text" id="address2" name="address2" placeholder="Unit or Apartment #"></input>
-                            <input type="text" id="city" name="city" placeholder="City"></input>
-                            <input type="text" id="state" name="state" placeholder="State"></input>
-                            <input type="text" id="zipcode" name="zipcode" placeholder="Zipcode"></input>
+                            <div className="delivery-info">
+                                <input type="text" id="address1" name="address1" placeholder="Street Address"></input>
+                                <input type="text" id="address2" name="address2" placeholder="Unit or Apartment #"></input>
+                                <input type="text" id="city" name="city" placeholder="City"></input><br/>
+                                <input type="text" id="state" name="state" placeholder="State"></input>
+                                <input type="text" id="zipcode" name="zipcode" placeholder="Zipcode"></input>
+                            </div>
+
+                            <div className="order-details-header">
+                                Payment Details
+                            </div>
+                            <input type="text" id="ccno" name="ccno" placeholder="Credit Card #"></input>
+                            <input type="text" id="ccexp" name="ccexp" placeholder="Expiration (MM/DD)"></input>
+                            <input type="text" id="cccvv" name="cccvv" placeholder="CCV"></input><br/>
+
+                            <input type="submit" value="Submit Order" className="submit-button"></input>
+
                         </form>
                     </div>
                     <div className="cart-items">
@@ -42,16 +54,16 @@ class Checkout extends React.Component {
                                 <td>$3.99</td>
                             </tr>
                             <tr>
-                                <td>Coffee</td>
-                                <td>$3.99</td>
+                                <td>Bagel</td>
+                                <td>$1.99</td>
                             </tr>
                             <tr>
-                                <td>Coffee</td>
-                                <td>$3.99</td>
+                                <td>Banana</td>
+                                <td>$0.99</td>
                             </tr>
                             <tr className="total-row">
                                 <td>Total</td>
-                                <td>$11.97</td>
+                                <td>$6.97</td>
                             </tr>
                         </table>
                     </div>
